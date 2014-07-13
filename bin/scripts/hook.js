05ac8c3e50ebb67535fe694bc029b2b0a6f423e9
Yehonal <hw.2@hotmail.it>
2014-07-14 01:46:26 +0200

    fixed hook script

--- bin/scripts/hook.js
+++ bin/scripts/hook.js
@@ -6,40 +6,16 @@
 var endpoints = process.argv;
 endpoints.splice(0, 2); // remove the first two parameters
 var type = endpoints.splice(0, 1); // type of event
-var exec = require('child_process').exec;
 var fs = require("fs");
+
+var basePath=process.cwd();
 
 for (var i = 0; i < endpoints.length; i++) {
 
-    var basePath=process.cwd();
     var modulePath = basePath + "/modules/" + endpoints[i];
     var postScriptFile = modulePath +"/.hw2/installer_hook.js"
 
-    fs.exists(postScriptFile, function(exists) {
-        if (exists) {
-
-            // exec mode, passing 3 arguments to the script: 
-            // * argv[2] type of event
-            // * argv[3] base path
-            // * argv[4] module path
-            /*exec('node ' + postScriptFile + " " + type + ' "' + basePath + '"' + ' "' + modulePath + '"',
-                    function(error, stdout, stderr) {
-                        console.log('stdout: ' + stdout);
-                        console.log('stderr: ' + stderr);
-                        if (error !== null) {
-                            console.log('exec error: ' + error);
-                        }
-                    }
-            );*/
-            
+    if (fs.existsSync(postScriptFile)) {
             require(postScriptFile)(type,basePath,modulePath);
-        } else {
-            console.log("file " + postScriptFile + " doesn't exists");
-        }
-    });
-
+    };
 }
-
-
-
-
